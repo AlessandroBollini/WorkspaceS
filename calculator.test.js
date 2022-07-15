f0b1ffd826191592a4cc4263e16aefa2ce1f058a
USERDOMAIN\bollini <bollini.alessandro1998@gmail.com>
2022-07-15 12:09:32 +0200

    commit calculator

---
+++ calculator.test.js
@@ -0,0 +1,10 @@
+const calculator=require('./calculator');
+
+test('Test for add method',()=>{
+    expect(calculator.add(2,3)).toBe(5);
+});
+
+test('Test for sub method',()=>{
+    expect(calculator.sub(10,1)).toBe(9);
+});
+
---
+++ index.js
@@ -0,0 +1,23 @@
+const calculator=require('./calculator');
+const express=require("express");
+let app=express();
+const parseUrl = require('body-parser');
+let encodeUrl = parseUrl.urlencoded({ extended: true });
+let results=[];
+module.exports.results=results;
+
+app.get('/calculator',(req,res)=>{
+    res.sendFile(__dirname+'/form.html');
+})
+
+app.post('/calculator',encodeUrl,(req,res)=>{
+    const x=parseInt(req.body.x);
+    const y=parseInt(req.body.y);
+    results.push("Sum is: "+calculator.add(x,y)+" difference is: "+calculator.sub(x,y));
+    res.sendFile(__dirname+'/form.html');
+})
+
+
+app.listen(3000,()=>{
+    console.log("App is listening on port 3000");
+})
